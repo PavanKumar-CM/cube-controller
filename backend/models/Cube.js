a666b60f7ca410030bfd7f2341580e47a4ec4692
PAVAN30627 <vvce21cse0055@vvce.ac.in>
2025-07-16 00:43:27 +0530

    Update: Schema

--- backend/models/Cube.js
+++ backend/models/Cube.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');SVGAnimatedAngle
+const mongoose = require('mongoose');
 
 const CubeSchema = new mongoose.Schema({
     cubeId: { type: String, default: 'cube_1' },
